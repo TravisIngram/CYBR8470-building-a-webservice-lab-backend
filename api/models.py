1b1e8a1a52b8cf658ff75b4355c1ffe3cc7c4197
TravisIngram <ingramtravis@gmail.com>
2021-01-26 19:25:31 -0600

    Add initial Breed model to api

--- api/models.py
+++ api/models.py
@@ -30,6 +30,38 @@ class ApiKey(models.Model):
 class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ('owner','key')
 
+# Breed Model
+class Breed(models.Model):
+    TINY = 'tiny'
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
+    BREED_SIZE_CHOICES = [
+        (TINY, 'tiny'),
+        (SMALL, 'small'),
+        (MEDIUM, 'medium'),
+        (LARGE, 'large'),
+    ]
+    size = models.CharField(
+        max_length=6,
+        choices=BREED_SIZE_CHOICES,
+        default=TINY,
+    )
+
+    name = models.CharField(max_length=50, blank=False)
+    # (a character string) [should accept Tiny, Small, Medium, Large]
+    friendliness = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    # (an integer field) [should accept values from 1-5]
+    trainability = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    # (an integer field) [should accept values from 1-5]
+    sheddingamount = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    # (an integer field) [should accept values from 1-5]
+    exerciseneeds = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    # (an integer field) [should accept values from 1-5]
+
+    def __str__(self):
+        return str(self.name)
+
 # Dog model
 class Dog(models.Model):
     name = models.CharField(max_length=50, blank=False)
