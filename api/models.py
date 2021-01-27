2a1f6e8f5410f15988799cf0ccb2a469741f6a06
TravisIngram <ingramtravis@gmail.com>
2021-01-26 18:34:25 -0600

    Add initial Dog model to api

--- api/models.py
+++ api/models.py
@@ -29,3 +29,16 @@ class ApiKey(models.Model):
 
 class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ('owner','key')
+
+# Dog model
+class Dog(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    age = models.IntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=20, blank=False)
+    color = models.CharField(max_length=30, blank=False)
+    favoriteFood = models.CharField(max_length=50, blank=False)
+    favoriteToy = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return str(self.name)
