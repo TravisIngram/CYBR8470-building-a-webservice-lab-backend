dcaddd4ddae310c4d85d97ea85f9f9452077ef19
TravisIngram <ingramtravis@gmail.com>
2021-02-02 15:29:16 -0600

    Added initial get, post methods for breedDetail, list

--- api/controllers.py
+++ api/controllers.py
@@ -125,9 +125,17 @@ class Events(APIView):
     renderer_classes = (renderers.JSONRenderer, )
 
     def get(self, request, format=None):
-        events = Event.objects.all()
-        json_data = serializers.serialize('json', events)
-        content = {'events': json_data}
+
+        # if not request.user.is_authenticated:
+        # return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+        if not request.user.is_authenticated:
+            return Response({'sucess':False, 'error':'User not authenticated.'})
+            # return render(request, 'myapp/login_error.html')
+        else:
+            events = Event.objects.all()
+            json_data = serializers.serialize('json', events)
+            content = {'events': json_data}
         return HttpResponse(json_data, content_type='json')
 
     def post(self, request, *args, **kwargs):
@@ -210,7 +218,8 @@ class DogDetail(APIView):
     renderer_classes = (renderers.JSONRenderer, )
 
     def get(self, request, format=None):
-        dogs = Dog.objects.all().filter(name = request.name)
+        dogs = Dog.objects.all()
+        # dogs = Dog.objects.all().filter(name = request.name)
         json_data = serializers.serialize('json', dogs)
         content = {'dogs': json_data}
         return HttpResponse(json_data, content_type='json')
@@ -220,14 +229,100 @@ class DogList(APIView):
     parser_classes = (parsers.JSONParser,parsers.FormParser)
     renderer_classes = (renderers.JSONRenderer, )
 
+    def get(self, request, format=None):
+        dogs = Dog.objects.all()
+        # dogs = Dog.objects.all().filter(name = request.name)
+        json_data = serializers.serialize('json', dogs)
+        content = {'dogs': json_data}
+        return HttpResponse(json_data, content_type='json')
+
+    def post(self, request, *args, **kwargs):
+        print 'REQUEST DATA'
+        print str(request.data)
+
+        name = request.data.get('name')
+        age = int(request.data.get('age'))
+        breed = request.data.get('breed')
+        gender = request.data.get('gender')
+        color = request.data.get('color')
+        favoriteFood = request.data.get('favoriteFood')
+        favoriteToy = request.data.get('favoriteToy')
+
+        requestor = request.META['REMOTE_ADDR']
+
+        newDog = Dog(
+            name=name,
+            age=age,
+            breed=breed_id,
+            color=color,
+            favoriteFood=favoriteFood,
+            favoriteToy=favoriteToy,
+            requestor=requestor
+        )
+
+        try:
+            newDog.clean_fields()
+        except ValidationError as e:
+            print e
+            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+
+        newDog.save()
+        print 'New Event Logged from: ' + requestor
+        return Response({'success': True}, status=status.HTTP_200_OK)
+
 # Breed routes
 
 class BreedDetail(APIView):
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser,parsers.FormParser)
-    renderer_classes = (renderers.JSONRenderer, )MaxValueValidator
+    renderer_classes = (renderers.JSONRenderer, )
+
+    def get(self, request, format=None):
+        # breeds = Breed.objects.all()
+        breeds = Breed.objects.all().filter(pk = request.pk)
+        json_data = serializers.serialize('json', breeds)
+        content = {'breeds': json_data}
+        return HttpResponse(json_data, content_type='json')
 
 class BreedList(APIView):
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser,parsers.FormParser)
     renderer_classes = (renderers.JSONRenderer, )
+
+    def get(self, request, format=None):
+        breeds = Breed.objects.all()
+        json_data = serializers.serialize('json', breeds)
+        content = {'breeds': json_data}
+        return HttpResponse(json_data, content_type='json')
+
+    def post(self, request, *args, **kwargs):
+        print 'REQUEST DATA'
+        print str(request.data)
+
+        name = request.data.get('name')
+        size = request.data.get('size')
+        friendliness = int(request.data.get('friendliness'))
+        trainability = int(request.data.get('trainability'))
+        sheddingamount = int(request.data.get('sheddingamount'))
+        exerciseneeds = int(request.data.get('exerciseneeds'))
+
+        requestor = request.META['REMOTE_ADDR']
+
+        newBreed = Breed(
+            name=name,
+            size=size,
+            friendliness=friendliness,
+            trainability=trainability,
+            sheddingamount=sheddingamount,
+            exerciseneeds=exerciseneeds
+        )
+
+        try:
+            newBreed.clean_fields()
+        except ValidationError as e:
+            print e
+            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+
+        newBreed.save()
+        print 'New Event Logged from: ' + requestor
+        return Response({'success': True}, status=status.HTTP_200_OK)
